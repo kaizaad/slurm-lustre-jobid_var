7efdccc0172892db2c64f6f0d0e4b6827c77813d
Kaizaad Bilimorya <kaizaad@computecanada.ca>
2021-05-06 18:58:03 -0400

    Format cleanup

--- slurm-lustre-jobid_var.c
+++ slurm-lustre-jobid_var.c
@@ -22,51 +22,53 @@
 int main(int argc, char** argv)
 {
 
-        char slurm_tuplet[MAXSIZE] = "";
-        char *slurm_job_id = NULL;
-        char *slurm_job_uid = NULL;
-        char *slurm_job_account = NULL;
-        size_t sbytes_leftover;
+	char slurm_tuplet[MAXSIZE] = "";
+	char *slurm_job_id = NULL;
+	char *slurm_job_uid = NULL;
+	char *slurm_job_account = NULL;
+	size_t sbytes_leftover;
 
-        char *st = NULL;
 
-        // Get the SLURM environment variables we are interested in
-        slurm_job_id = getenv("SLURM_JOB_ID");
-        slurm_job_uid = getenv("SLURM_JOB_UID");
-        slurm_job_account = getenv("SLURM_JOB_ACCOUNT");
+	// Get the SLURM environment variables we are interested in
+	slurm_job_id = getenv("SLURM_JOB_ID");
+	slurm_job_uid = getenv("SLURM_JOB_UID");
+	slurm_job_account = getenv("SLURM_JOB_ACCOUNT");
+	
+	/* 
+	 * Concatenate the environment strings making sure we don't exceed MAXSIZE
+	 * The strlen() + delimiter should be < MAXSIZE - NULL terminator
+	 */
+	if (slurm_job_id == NULL || (strlen(slurm_job_id) + 1 > MAXSIZE - 1)) {
+		fprintf(stderr,"SLURM_JOB_ID is either NULL or too long to use!\n");
+		return (EXIT_FAILURE);
+	} else {
+		strcpy(slurm_tuplet,slurm_job_id);
+		strcat(slurm_tuplet, ".");
+	}
 
-        /* 
-         * Concatenate the environment strings making sure we don't exceed MAXSIZE
-         * The strlen() + delimiter should be < MAXSIZE - NULL terminator
-         */
-        if (slurm_job_id == NULL || (strlen(slurm_job_id) + 1 > MAXSIZE - 1)) {
-                fprintf(stderr,"SLURM_JOB_ID is either NULL or too long to use!\n");
-                return (EXIT_FAILURE);
-        } else {
-                strcpy(slurm_tuplet,slurm_job_id);
-                strcat(slurm_tuplet, ".");
-        }
+	if (slurm_job_uid == NULL || (strlen(slurm_tuplet) + strlen(slurm_job_uid) + 1 > MAXSIZE - 1)) {
+		fprintf(stderr,"SLURM_JOB_UID is either NULL or too long to use!\n");
+		return (EXIT_FAILURE);
+	} else {
+		strcat(slurm_tuplet, slurm_job_uid);
+		strcat(slurm_tuplet, ".");
+	}
 
-        if (slurm_job_uid == NULL || (strlen(slurm_tuplet) + strlen(slurm_job_uid) + 1 > MAXSIZE - 1)) {
-                fprintf(stderr,"SLURM_JOB_UID is either NULL or too long to use!\n");
-                return (EXIT_FAILURE);
-        } else {
-                strcat(slurm_tuplet, slurm_job_uid);
-                strcat(slurm_tuplet, ".");
-        }
-
-        // Get the # of bytes to add to keep slurm_tuplet <= MAXSIZE - 1
-        if (slurm_job_account == NULL) {
-                fprintf(stderr,"SLURM_JOB_ACCOUNT is NULL!\n");
+	// Get the # of bytes to add to keep slurm_tuplet <= MAXSIZE - 1
+	if (slurm_job_account == NULL) {
+		fprintf(stderr,"SLURM_JOB_ACCOUNT is NULL!\n");
         return (EXIT_FAILURE);
-        } else {
-                sbytes_leftover = (MAXSIZE - 1) - (strlen(slurm_tuplet));
-                strncat(slurm_tuplet,slurm_job_account,sbytes_leftover);
-        }
+    } else {
+		sbytes_leftover = (MAXSIZE - 1) - (strlen(slurm_tuplet));
+		strncat(slurm_tuplet,slurm_job_account,sbytes_leftover);
+	}
 
-        fprintf(stdout,"SLURM_TUPLET is : %s\n", slurm_tuplet);
-        fprintf(stdout,"SLURM_TUPLET lenght is : %lu\n", strlen(slurm_tuplet));
+	fprintf(stdout,"SLURM_TUPLET is : %s\n", slurm_tuplet);
+	fprintf(stdout,"SLURM_TUPLET lenght is : %lu\n", strlen(slurm_tuplet));
 
-        setenv("SLURM_TUPLET",slurm_tuplet,0);
-        fprintf(stdout,"The SLURM_TUPLET from environment is : %s\n", getenv("SLURM_TUPLET"));
+	setenv("SLURM_TUPLET",slurm_tuplet,0);
+	fprintf(stdout,"The SLURM_TUPLET from environment is : %s\n", getenv("SLURM_TUPLET"));
+
+	return(0);
+
 }
